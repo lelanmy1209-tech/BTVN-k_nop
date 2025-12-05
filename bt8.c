900e6f7df1d819b6e9bd74d0ca32b90035d2c601
lelanmy1209-tech <lelanmy1209@gmail.com>
2025-12-05 20:44:29 +0700

    Update bt8.c

--- bt8.c
+++ bt8.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
 int main () {
-
+      int thang, nam ;
+	printf("Nhap thang: ");
+    scanf("%d", &thang);
+    printf("Nhap nam: ");
+    scanf("%d", &nam);
+	
+	if ( thang >= 1 && thang <= 12) {
+		if (thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12) {
+			printf("Thang %d co 31 ngay",thang);
+		}
+		else if ( thang == 4 || thang == 6 || thang == 9 || thang == 11) {
+			printf("Thang %d co 30 ngay", thang);
+		}
+	    else {
+		    if ( nam % 400 == 0 || (nam % 4 == 0 && nam % 100 != 0 )) {
+		     	printf("nam %d, thang %d co 29 ngay", nam, thang);
+		    }else {
+		     	printf("nam %d, thang %d co 28 ngay", nam , thang);
+		}	
+	}}
+	else {
+		printf("Thang nhap k hop le");
+	}
 
 
 
