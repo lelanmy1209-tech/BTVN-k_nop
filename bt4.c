bdb59d984d34230b9447e55395a8116df24d1e04
lelanmy1209-tech <lelanmy1209@gmail.com>
2025-12-07 19:54:31 +0700

    Update bt4.c

--- bt4.c
+++ bt4.c
@@ -7,7 +7,7 @@ int main() {
       printf("Nhập cạnh a, b và c: ");
       scanf("%d%d%d", &a, &b, &c) ;
 
-      if ( a + b > c || b + c > a || a + c > b) {
+      if ( a + b > c && b + c > a && a + c > b) {
             float p = ( a + b + c) / 2.0;
             float s = sqrt(p * ( p - a) * ( p - b) * ( p -c));
             printf("Diện tích tam giác là: %f", s);
